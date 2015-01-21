2bd23f788d306eaac53ea415be958fcd21a05f0f
vanderwijk <vanderwijk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-21 15:22:30 +0000

    Branch 2.7.4
    git-svn-id: https://plugins.svn.wordpress.org/custom-post-widget/tags/2.7.4@1072641 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- custom-post-widget.php
+++ custom-post-widget.php
@@ -1,16 +1,16 @@
 <?php
 /*
- Plugin Name: Custom Post Widget
- Plugin URI: http://www.vanderwijk.com/wordpress/wordpress-custom-post-widget/
+ Plugin Name: Savvii Custom Post Widget
+ Plugin URI: https://www.savvii.eu/wordpress-custom-post-widget/
  Description: Show the content of a custom post of the type 'content_block' in a widget or with a shortcode.
- Version: 2.3.3
- Author: Johan van der Wijk
- Author URI: http://www.vanderwijk.com
+ Version: 2.7.4
+ Author: Savvii WordPress Hosting
+ Author URI: https://www.savvii.eu/
  License: GPL2
 
- Release notes: Version 2.3 Various bugfixes and improvements
+ Release notes: Version 2.7.4 The shortcode now includes the slug of the content block
  
- Copyright 2013 Johan van der Wijk (email: info@vanderwijk.com)
+ Copyright 2014 Johan van der Wijk (email: info@vanderwijk.com)
  
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License, version 2, as 
@@ -38,6 +38,10 @@ function custom_post_widget_plugin_init() {
 
 // Loads the widgets packaged with the plugin.
 function custom_post_widget_load_widgets() {
-	require( 'post-widget.php' );
+	require_once( 'post-widget.php' );
 	register_widget( 'custom_post_widget' );
 }
+
+require_once( 'meta-box.php' );
+require_once( 'popup.php' );
+require_once( 'notice.php' );
